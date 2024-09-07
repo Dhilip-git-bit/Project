bf415e88144dd46d52485e9e35368173197c3067
Dhilip-git-bit <dhilipjj1@gmail.com>
2024-09-07 17:11:04 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-13b8bfv1#7&hv4274x614%jx7xn%9og-(32qd0vpg%i)wjv6_9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.4']
 
 
 # Application definition
